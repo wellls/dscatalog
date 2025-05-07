794637248610c1230a5ccf3ba75aa5f117f2ccc8
Wellington <wellington.map@hotmail.com>
2025-05-07 09:31:32 -0300

    feat: implement Category entity

---
+++ src/main/java/com/github/wellls/dscatalog/controllers/CategoryController.java
@@ -0,0 +1,21 @@
+package com.github.wellls.dscatalog.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.wellls.dscatalog.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = List.of(new Category(1L, "Books"), new Category(2L, "Electronics"));
+        return ResponseEntity.ok().body(list);
+    }
+}
